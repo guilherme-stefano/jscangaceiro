3fa740051d08be56e559821854da05d4d4b72c98
guilherme-stefano <guilhermenostefano@gmail.com>
2018-10-28 21:40:01 -0300

    initial commit

---
+++ client/app/domain/negociacao/Negociacao.js
@@ -0,0 +1,24 @@
+class Negociacao {
+	//	MUDANÇA	NO	NOME	DOS	PARÂMETROS,	AGORA	COM	UNDERLINE
+	constructor(_data,	_quantidade,	_valor)	{
+	Object.assign(this,	{	_quantidade,	_valor	});
+	this._data	=	new Date(_data.getTime());
+	Object.freeze(this);
+	}
+
+	getVolume()	{
+		return this._quantidade	*	this._valor;
+	}
+
+	getData() {
+		return new Date(this._data.getTime());
+	}
+
+	getQuantidade() {
+		return this._quantidade;
+	}
+
+	getValor() {
+		return this._valor;
+	}
+}
