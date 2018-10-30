a8fde423c877c459d766deace1616f61cf120b67
guilherme-stefano <guilhermenostefano@gmail.com>
2018-10-29 23:28:47 -0300

    adicionada controller

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -1,2 +1,20 @@
 class	NegociacaoController	{
+	constructor() {
+	//	a	ideia	é	que	$	seja	o	querySelector
+		let	$	=	document.querySelector.bind(document);
+			this._inputData	=	$('#data');
+			this._inputQuantidade	=	$('#quantidade');
+			this._inputValor	=	$('#valor');
+	}
+
+	adiciona(event){
+		event.preventDefault();
+		let	data	=	new Date(...this._inputData.value.split('-'));
+		let	negociacao	=	new	Negociacao(
+		this._inputData.value,
+		parseInt(this._inputQuantidade.value),
+		parseFloat(this._inputValor.value)
+		);
+		console.log(negociacao);
+	}
 }
